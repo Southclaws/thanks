db64db3b09eaffef69d24f5a476a7f440ff5b7f9
Southclaws <southclawjk@gmail.com>
2018-02-10 11:31:17 +0000

    goreleaser

--- lists.go
+++ lists.go
@@ -1,4 +1,6 @@
 package main
 
 // PackagesTakingForDonations is a map from Go import path to donation page
-var PackagesTakingForDonations = map[string]string{}
+var PackagesTakingForDonations = map[string]string{
+	"github.com/goreleaser/goreleaser": "https://opencollective.com/goreleaser/donate",
+}
