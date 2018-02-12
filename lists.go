eccda68555d1c96b31b74541c9f2dcfc3b1b369a
Southclaws <southclawjk@gmail.com>
2018-02-12 16:23:05 +0000

    added iris, fabio and colly

--- lists.go
+++ lists.go
@@ -3,5 +3,8 @@ package main
 // PackagesTakingForDonations is a map from Go import path to donation page
 var PackagesTakingForDonations = map[string]string{
 	"github.com/goreleaser/goreleaser": "https://opencollective.com/goreleaser/donate",
-	"github.com/go-vgo/robotgo": "https://www.paypal.me/veni0/25",
+	"github.com/go-vgo/robotgo":        "https://www.paypal.me/veni0/25",
+	"github.com/kataras/iris":          "https://opencollective.com/iris/donate",
+	"github.com/fabiolb/fabio":         "https://opencollective.com/fabio/donate",
+	"github.com/gocolly/colly":         "https://opencollective.com/colly/donate",
 }
