67a82b85ae34b6ba280ac93bbb8e05dcda43d93f
Southclaws <southclawjk@gmail.com>
2018-02-12 16:05:35 +0000

    add go-vgo/robotgo

--- lists.go
+++ lists.go
@@ -3,4 +3,5 @@ package main
 // PackagesTakingForDonations is a map from Go import path to donation page
 var PackagesTakingForDonations = map[string]string{
 	"github.com/goreleaser/goreleaser": "https://opencollective.com/goreleaser/donate",
+	"github.com/go-vgo/robotgo": "https://www.paypal.me/veni0/25",
 }
